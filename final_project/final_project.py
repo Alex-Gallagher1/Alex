4934ecac05a394972876c90a406d159546d6548c
Alex Gallagher <Alexander_gallagher@berkeley.edu>
2025-11-12 17:00:12 -0800

    Check in before I switch

---
+++ final_project/final_project.py
@@ -0,0 +1,8 @@
+#File: final_project.py
+import numpy as np
+import matplotlib.pyplot as plt
+from steputils import p21
+
+myshape = p21.load_step_file("Python_Final_Project.step")
+
+print(myshape)
