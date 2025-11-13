8c154f106f4b30486f82689369cfe9d4b6c6e19d
Alex Gallagher <Alexander_gallagher@berkeley.edu>
2025-11-12 21:11:55 -0800

    saving

--- final_project/final_project.py
+++
@@ -1,8 +0,0 @@
-#File: final_project.py
-import numpy as np
-import matplotlib.pyplot as plt
-from steputils import p21
-
-myshape = p21.load_step_file("Python_Final_Project.step")
-
-print(myshape)
